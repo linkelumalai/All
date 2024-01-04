6dfd54ae5651222cd2e8c3384a24a610d4597493
linkelumalai <linkelumalai@gmail.com>
2024-01-04 22:15:13 +0530

    spring boot applications

---
+++ spring_microservice/amz-cart/src/main/java/com/example/demo/AmzCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmzCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmzCartApplication.class, args);
+	}
+
+}
